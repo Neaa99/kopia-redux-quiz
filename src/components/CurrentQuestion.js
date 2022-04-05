7aca2ab2482afec0caaa625b93e35f48aeec1da7
Neaa99 <linnea@kringelkroken.se>
2022-04-05 19:40:16 +0200

    test

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,35 +1,53 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { quiz } from 'reducers/quiz'
+import { Summary } from './Summary'
+import { ProgressBar } from './ProgressBar'
+import { Header } from './Header'
+import { Footer } from './Footer'
+
+
 
 export const CurrentQuestion = () => {
   // Getting data from the store
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const quizOver = useSelector((state) => state.quiz.quizOver)
   const store = useSelector((state) => state)
   console.log(store)
 
   // Forwarding data to the store / update'ing
   const dispatch = useDispatch()
   const onAnswerSubmit = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({questionId:id, answerIndex:index }))
-    if(question.correctAnswerIndex === index) {
-      dispatch(quiz.actions.goToNextQuestion())
-    }
-
+    dispatch(quiz.actions.submitAnswer({questionId:id, answerIndex:index}));
+    dispatch(quiz.actions.goToNextQuestion())
+    
   }
-
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
+
   return (
     <div>
-      <h1>Question: {question.questionText}</h1>
-      {question.options.map((item, index) => {
-        return <button onClick={() => onAnswerSubmit(question.id, index)}key={item}>{item}</button>
-      })}
+      <Header />
+      
+
+      {quizOver ? <Summary /> : (
+        <div className="current-question-container">
+          <h1>Question: {question.questionText}</h1>
+            {question.options.map((item, index) => (
+               <button 
+                onClick={() => onAnswerSubmit(question.id, index)} 
+                key={item}>
+                {item}
+                </button>
+            ))}
+          <ProgressBar />
+        </div>
+      )}
+
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+
+    return (
+        <>
+            <h1>Stranger Things quiz</h1>
+            <img src='https://i.postimg.cc/tRhdkhk0/STLOGO.png' width="500px" className="logo"></img>
+        </>
+    )
+}
